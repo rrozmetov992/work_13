b9a8f74b57beadab9db9177cdeb6e22d9a0e17e7
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-11 08:17:17 +0300

    Add files via upload

---
+++ work_13.py
@@ -0,0 +1,50 @@
+# Boolean 19
+# a,b,c=map(int, input().split())
+# d1=a==(-b)
+# d2=a==(-c)
+# d3=b==(-c)
+# if d1 or d2 or d3:
+#     print("bir jufti qarama qarshi")
+# else:
+#     print("vindin amas")
+#Boolean 20
+# son=int(input("son="))
+# a=son%10
+# b=son//10%10
+# c=son//100
+# t=a!=b and b!=c and c!=a
+# print(t)
+#Boolean 21
+# son=int(input("son="))
+# a=son%10
+# b=son//10%10
+# c=son//100
+# t=a==b+1==c+2
+# print(t)
+#Boolean 22
+# son=int(input("son="))
+# a=son%10
+# b=son//10%10
+# c=son//100
+# t=a==b+1==c+2 or a==b-1==c-2
+# print(t)
+#Boolean 23
+# son=int(input("son="))
+# a=son%10
+# b=son//10%10
+# c=son//100
+# t=a==b==c
+# print(t)
+#Boolean 24
+# A,B,C=map(int, input().split())
+# D=B**2-4*A*C
+# t=D>=0
+# print(t)
+#Boolean 25
+# x,y=map(float, input().split())
+# t=x<0 and y>0
+# print(t)
+#Boolean 26
+# x,y=map(float, input().split())
+# t=x>0 and y<0
+# print(t)
